f2b484ad6e6737ffc846da16e6d59a17934516ec
thivav <thivaharan28@gmail.com>
2024-08-06 21:35:37 +0530

    model build completed

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from src.kidney_disease_classification import logger
 from src.kidney_disease_classification.pipeline.data_ingestion import DataIngestionTrainingPipeline
+from src.kidney_disease_classification.pipeline.prepare_base_model import PrepareBaseModelTrainingPipeline
 
-STAGE_NAME = "Data Ingestion Stage"
+STAGE_NAME = 'Data ingestion stage'
 
 try:
     logger.info(f'------- stage {STAGE_NAME} started -------')
     obj=DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f'------- stage {STAGE_NAME} completed -------')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Prepare base model'
+
+try:
+    logger.info(f'------------ stage {STAGE_NAME} started ------------------')
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f'------------ stage {STAGE_NAME} completed ----------------')
 except Exception as e:
     logger.exception(e)
     raise e
